6729fb9f05c0e3f5c4abcb0f46fc64e38b439f6a
Dillon Mulroy <dillon.mulroy@gmail.com>
2023-10-03 18:23:28 -0400

    update main

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,9 @@
-let () = print_endline "Hello, World!"
+[@@@ocaml.warning "-26"]
+
+let () =
+  let client =
+    Vexilla.Client.make ~environment:"test-env"
+      ~base_url:(Uri.of_string "http://localhost:8080")
+      ~instance_id:"test-id" ()
+  in
+  ()
