f1e94e55aa122397c9fdda4f7349b3446179c957
Dillon Mulroy <dillon.mulroy@gmail.com>
2023-10-03 18:10:20 -0400

    refactor to types module

--- lib/client.ml
+++ lib/client.ml
@@ -1,3 +1,5 @@
+open Types
+
 type environment_lookup_table =
   (Group.id, Environment.id, Environment.name) Lookup.Composite_table.t
 
@@ -41,3 +43,14 @@ let set_manifest client (manifest : Manifest.t) =
            (group.id, group.name))
     |> Lookup.Table.add_list group_lookup_table;
     Ok { client with manifest; group_lookup_table }
+
+(* let latest_manifest_version = "1.0"
+   let empty = { version = latest_manifest_version; groups = [] } *)
+
+(* let get ~(base_url : Uri.t) ~(fetch_hook : (t, Error.t) Fetch.hook) : t Lwt.t =
+   let* result = Uri.with_path base_url "manifest.json" |> fetch_hook in
+   match result with
+   | Ok manifest -> Lwt.return manifest
+   | Error err ->
+       Fmt.pr "Error: failed to fetch manifest: %s\n%!" (Error.to_string err);
+       Lwt.return empty *)
