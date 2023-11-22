18939856ad2b1c3dbade0788d74f641ff1fdc5bb
Dillon Mulroy <dillon.mulroy@gmail.com>
2023-11-21 22:10:38 -0500

    Add yojson to types.ml

--- lib/schedule.ml
+++ lib/schedule.ml
@@ -10,58 +10,59 @@ let is_schedule_active_with_now ~schedule ~schedule_type now =
   | Environment | Global -> (
       (* Convert to/from a date to zero out the time to the beginning of the day *)
       let@ beginning_of_start_date =
-        schedule.start |> Ptime.to_date |> Ptime.of_date
+        schedule.start |> Date_time.to_date |> Date_time.of_date
         |> Option.to_result ~none:`Invalid_date
       in
       let@ ending_of_end_date =
-        schedule.end' |> Ptime.to_date |> fun date ->
-        Ptime.of_date_time (date, ((23, 59, 59), 0))
+        schedule.end' |> Date_time.to_date |> fun date ->
+        Date_time.of_date_time (date, ((23, 59, 59), 0))
         |> Option.to_result ~none:`Invalid_date
       in
       if
-        Ptime.is_earlier now ~than:beginning_of_start_date
-        || Ptime.is_later now ~than:ending_of_end_date
+        Date_time.is_earlier now ~than:beginning_of_start_date
+        || Date_time.is_later now ~than:ending_of_end_date
       then Ok false
       else
-        let _, start_time = Ptime.to_date_time schedule.start_time in
-        let _, end_time = Ptime.to_date_time schedule.end_time in
+        let _, start_time = Date_time.to_date_time schedule.start_time in
+        let _, end_time = Date_time.to_date_time schedule.end_time in
         match schedule.time_type with
         | None -> Ok true
         | Start_end ->
             let@ start =
-              Ptime.(of_date_time (to_date beginning_of_start_date, start_time))
+              Date_time.(
+                of_date_time (to_date beginning_of_start_date, start_time))
               |> Option.to_result ~none:`Invalid_date
             in
             let@ end' =
-              Ptime.(of_date_time (to_date ending_of_end_date, end_time))
+              Date_time.(of_date_time (to_date ending_of_end_date, end_time))
               |> Option.to_result ~none:`Invalid_date
             in
-            let is_after_start_date_time = Ptime.is_later ~than:start now in
-            let is_before_end_date_time = Ptime.is_earlier ~than:end' now in
+            let is_after_start_date_time = Date_time.is_later ~than:start now in
+            let is_before_end_date_time = Date_time.is_earlier ~than:end' now in
             Ok (is_after_start_date_time && is_before_end_date_time)
         | Daily ->
-            let now = Ptime_clock.now () in
+            let now = Date_time.Clock.now () in
             let@ today_zero_timestamp =
-              Ptime.to_date now |> Ptime.of_date
+              Date_time.to_date now |> Date_time.of_date
               |> Option.to_result ~none:`Invalid_date
             in
             let@ zeroed_start_timestamp =
-              Ptime.(of_date_time (to_date epoch, start_time))
+              Date_time.(of_date_time (to_date epoch, start_time))
               |> Option.to_result ~none:`Invalid_date
             in
             let@ zeroed_end_timestamp =
-              Ptime.(of_date_time (to_date epoch, end_time))
+              Date_time.(of_date_time (to_date epoch, end_time))
               |> Option.to_result ~none:`Invalid_date
             in
-            let zeroed_end_span = Ptime.to_span zeroed_end_timestamp in
-            let day_span = Ptime.Span.of_int_s 86400 in
+            let zeroed_end_span = Date_time.to_span zeroed_end_timestamp in
+            let day_span = Date_time.Span.of_int_s 86400 in
             let@ zeroed_end_timestamp_plus_day =
-              Ptime.Span.add zeroed_end_span day_span
-              |> Ptime.of_span
+              Date_time.Span.add zeroed_end_span day_span
+              |> Date_time.of_span
               |> Option.to_result ~none:`Invalid_date
             in
             let@ start_timestamp =
-              Ptime.(
+              Date_time.(
                 Span.add
                   (to_span today_zero_timestamp)
                   (to_span zeroed_start_timestamp)
@@ -70,17 +71,18 @@ let is_schedule_active_with_now ~schedule ~schedule_type now =
             in
             let@ end_timestamp =
               if
-                Ptime.is_later ~than:zeroed_end_timestamp zeroed_start_timestamp
-                || Ptime.to_float_s zeroed_start_timestamp < 0.0
+                Date_time.is_later ~than:zeroed_end_timestamp
+                  zeroed_start_timestamp
+                || Date_time.to_float_s zeroed_start_timestamp < 0.0
               then
-                Ptime.(
+                Date_time.(
                   Span.add
                     (to_span today_zero_timestamp)
                     (to_span zeroed_end_timestamp_plus_day)
                   |> of_span)
                 |> Option.to_result ~none:`Invalid_date
               else
-                Ptime.(
+                Date_time.(
                   Span.add
                     (to_span today_zero_timestamp)
                     (to_span zeroed_end_timestamp)
@@ -88,12 +90,12 @@ let is_schedule_active_with_now ~schedule ~schedule_type now =
                 |> Option.to_result ~none:`Invalid_date
             in
             Ok
-              Ptime.(
+              Date_time.(
                 to_float_s now > to_float_s start_timestamp
                 && to_float_s now < to_float_s end_timestamp))
 
 let is_schedule_active ~schedule ~schedule_type =
-  is_schedule_active_with_now ~schedule ~schedule_type (Ptime_clock.now ())
+  is_schedule_active_with_now ~schedule ~schedule_type (Date_time.Clock.now ())
 
 let is_schedule_feature_active (feature : Types.Feature.t) =
   let attributes = Types.Feature.attributes feature in
